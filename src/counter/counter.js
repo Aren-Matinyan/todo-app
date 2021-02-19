88fa7a573edea5b9459253f7702b2bed379f87a2
Aren Matinyan <arenmat55@gmail.com>
2021-02-19 05:56:11 +0400

    using redux

---
+++ src/counter/counter.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import ChangeCount from "./change-count"
+import ShowCount from "./show-count"
+
+function Counter() {
+
+    return (
+        <div>
+            <ChangeCount/>
+            <ShowCount/>
+        </div>
+    )
+}
+
+export default Counter
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './Components/app/app';
+import {Provider} from 'react-redux'
+import Counter from "./counter/counter"
+import {store} from "./counter/store"
+// import App from './Components/app/app'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <Counter/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
